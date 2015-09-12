94fbe47fdcba0686f9e4f13736c31e01fb41b8d0
Mingyao Liu <zodiacsoulmate@gmail.com>
2015-09-12 16:14:10 -0400

    add dropdown picture grid

--- resources/js/components/dropdown.js
+++ resources/js/components/dropdown.js
@@ -9,40 +9,99 @@ var Dropdown = function($parent, data, type) {
   var buttonList = '';
 
   // LOOP THROUGH DATA & CREATE BUTTONS
-
-  var ItemList;
-  $.ajax({
-    method: "GET",
-    async: false,
-    url: "http://api.walmartlabs.com/v1/search?query=desk&format=json&apiKey=3dvh2s8d9fpnxskz7n483scn",
-    success: function(response){
-      ItemList = response.items;
-    }
-  })
-
-  for(var i = 0; i < ItemList.length; i++) {
+  for(var i = 0; i < data.length; i++) {
     buttonList = buttonList + liTemplate({
-      name: ItemList[i].itemId,
-      description: ItemList[i].name,
-      background: ItemList[i].background,
-      icon: ItemList[i].name,
-      type: type,
-      thumbnail : ItemList[i].thumbnailImage
+      name: data[i].name,
+      description: data[i].description,
+      background: data[i].background,
+      icon: data[i].icon,
+      type: type
     });
-
-    //$('.dropdown-button-icon', $(this)).css("background", "url(" + ItemList[i].thumbnailImage + ") center center no-repeat");
-
-
   }
+
+
+
+  
+
     // ADD DROPDOWN TO DOM
   $parent.append(ListTemplate({items: buttonList}));
 
-  $(".dropdown-button-icon", $('.dropdown-items')).each( function(index) {
-    if(index < ItemList.length) {
-    var av = $($(".dropdown-button-icon", $('.dropdown-items'))[index]);
-    av.css("background", "url(" + ItemList[index].thumbnailImage + ") center center no-repeat");
-  }
-  })
+
+  var ItemList;
+    $.ajax({
+      method: "GET",
+      async: false,
+      url: "http://api.walmartlabs.com/v1/search?query=desk&format=json&apiKey=3dvh2s8d9fpnxskz7n483scn",
+      success: function(response){
+        ItemList = response.items;
+      }
+    })
+
+    for(var i = 0; i < ItemList.length; i++) {
+      var deskNode = $('a[data-name=DESK]');
+      $( "<div class='imageLs' style='background: url(" + ItemList[i].thumbnailImage + ") no-repeat;background-size: cover;'></div>" ).insertAfter( deskNode );
+    }
+
+
+    $.ajax({
+      method: "GET",
+      async: false,
+      url: "http://api.walmartlabs.com/v1/search?query=chair&format=json&apiKey=3dvh2s8d9fpnxskz7n483scn",
+      success: function(response){
+        ItemList = response.items;
+      }
+    })
+
+    for(var i = 0; i < ItemList.length; i++) {
+      var deskNode = $('a[data-name=CHAIR]');
+      $( "<div class='imageLs' style='background: url(" + ItemList[i].thumbnailImage + ") no-repeat;background-size: cover;'></div>" ).insertAfter( deskNode );
+    }
+
+
+
+    $.ajax({
+      method: "GET",
+      async: false,
+      url: "http://api.walmartlabs.com/v1/search?query=Lamp&format=json&apiKey=3dvh2s8d9fpnxskz7n483scn",
+      success: function(response){
+        ItemList = response.items;
+      }
+    })
+
+    for(var i = 0; i < ItemList.length; i++) {
+      var deskNode = $('a[data-name=LAMP]');
+      $( "<div class='imageLs' style='background: url(" + ItemList[i].thumbnailImage + ") no-repeat;background-size: cover;'></div>" ).insertAfter( deskNode );
+    }
+
+
+
+    $.ajax({
+      method: "GET",
+      async: false,
+      url: "http://api.walmartlabs.com/v1/search?query=Bed&format=json&apiKey=3dvh2s8d9fpnxskz7n483scn",
+      success: function(response){
+        ItemList = response.items;
+      }
+    })
+
+    for(var i = 0; i < ItemList.length; i++) {
+      var deskNode = $('a[data-name=BED]');
+      $( "<div class='imageLs' style='background: url(" + ItemList[i].thumbnailImage + ") no-repeat;background-size: cover;'></div>" ).insertAfter( deskNode );
+    }
+
+
+
+      //$('.dropdown-button-icon', $(this)).css("background", "url(" + ItemList[i].thumbnailImage + ") center center no-repeat");
+
+
+
+
+  // $(".dropdown-button-icon", $('.dropdown-items')).each( function(index) {
+  //   if(index < ItemList.length) {
+  //   var av = $($(".dropdown-button-icon", $('.dropdown-items'))[index]);
+  //   av.css("background", "url(" + ItemList[index].thumbnailImage + ") center center no-repeat");
+  // }
+  // })
 
   // $(".dropdown-items li").each(function(index) {
 
--- resources/js/helpers/data.js
+++ resources/js/helpers/data.js
@@ -29,64 +29,24 @@ var data = {
 
   furniture: [
     {
-      name: 'android',
-      description: 'Android Toy',
-      icon: 'icon-android'
-    },
-    {
-      name: 'ballpit',
-      description: 'Ball Pit Pool',
-      icon: 'icon-ballpit'
-    },
-    {
-      name: 'desk',
-      description: 'Office Desk',
+      name: 'DESK',
+      description: 'Desks',
       icon: 'icon-desk'
     },
     {
-      name: 'dog_corgi',
-      description: 'Dog (Corgi)',
+      name: 'CHAIR',
+      description: 'Chairs',
+      icon: 'icon-ballpit'
+    },
+    {
+      name: 'LAMP',
+      description: 'Lamp',
+      icon: 'icon-android'
+    },
+    {
+      name: 'BED',
+      description: 'Bed',
       icon: 'icon-dog'
-    },
-    {
-      name: 'dog_retriever',
-      description: 'Dog (Retriever)',
-      icon: 'icon-dog'
-    },
-    {
-      name: 'laptop',
-      description: 'Laptop',
-      icon: 'icon-laptop'
-    },
-    {
-      name: 'nerfgun',
-      description: 'Nerfgun Pistol',
-      icon: 'icon-nerfgun'
-    },
-    {
-      name: 'pacman',
-      description: 'Pacman Arcade',
-      icon: 'icon-game'
-    },
-    {
-      name: 'pingpong',
-      description: 'Ping Pong Table',
-      icon: 'icon-pingpong'
-    },
-    {
-      name: 'plant1',
-      description: 'Plant (Shrub)',
-      icon: 'icon-plant'
-    },
-    {
-      name: 'plant2',
-      description: 'Plant (Succulent)',
-      icon: 'icon-plant'
-    },
-    {
-      name: 'redstapler',
-      description: 'Red Stapler',
-      icon: 'icon-stapler'
     }
   ]
 };
