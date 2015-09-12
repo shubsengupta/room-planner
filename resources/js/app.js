3cb13f67e00d9a6370a61d4934694de8405e52e8
Shubhagata Sengupta <shubsengupta@gmail.com>
2015-09-12 09:39:40 -0400

    add two step render to canvas image

--- resources/js/app.js
+++ resources/js/app.js
@@ -277,10 +277,24 @@ function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
         var img = new Image();
+
         img.onload = function(){
+
+            var oc = document.createElement('canvas'), octx = oc.getContext('2d');
+            
+            oc.width = img.width * 0.5;
+            oc.height = img.height * 0.5;
+            octx.drawImage(img, 0,0, oc.width,oc.height);
+
+            /// step 2
+            octx.drawImage(oc,0,0,oc.width * 0.5,oc.height * 0.5);
+            
             img.width = canvas.width;
             img.height = canvas.height;
-            ctx.drawImage(img,0,0,canvas.width, canvas.height);
+
+            ctx.drawImage(oc,0,0,oc.width * 0.5, oc.height * 0.5,
+                             0,0,canvas.width,canvas.height);
+
         }
         img.src = event.target.result;
     }
